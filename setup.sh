6c742b1cce5955713e329438b5ef7e254bd289cd
Jose Veliz <jose.veliz.francia@gmail.com>
2021-05-09 19:27:26 -0500

    add script to install stuff when reset my macbook

---
+++ setup.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+## Instructions
+# 1. Install fonts
+
+## Global Variables (empty for now)
+
+## HOMEBREW
+
+# Requirements and installation
+xcode-select --install
+curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh
+
+## Alfred 4
+brew install alfred
+## Hyperswitch
+brew install hyperswitch
+## Spectacle
+brew install spectacle
+## Explorer - Chrome - Firefox - Brave
+brew install chrome
+brew install firefox
+brew install brave
+
+## Discord
+brew install discord
+
+## Iterm 2
+brew install iterm2
+## NVIM 5
+brew install --HEAD neovim
+
+## Creation of files
+
+# .zprofile
+cd $HOME/
+touch .zprofile
+cat << EOF > .zprofile
+export EDITOR="nvim"
+export ZDOTDIR="$HOME/.config/zsh"
+alias vim="nvim"
+
+
