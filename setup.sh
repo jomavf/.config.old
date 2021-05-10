b1cf696b3596a42736d15f3cfda89b38c09ff441
Jose Veliz <jose.veliz.francia@gmail.com>
2021-05-09 19:58:58 -0500

    try to send output error message to trash

--- setup.sh
+++ setup.sh
@@ -8,7 +8,7 @@
 ## HOMEBREW
 
 # Requirements and installation
-xcode-select --install 2>&1 | grep -v "already installed and latest version"
+xcode-select --install >/dev/null 2>&1 
 curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh
 
 ## Alfred 4
