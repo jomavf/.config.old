cf546b247b07bb545a273286d2fac3695915f723
Jose Veliz <jose.veliz.francia@gmail.com>
2021-05-09 22:00:04 -0500

    update some conf and setup nvm

--- setup.sh
+++ setup.sh
@@ -21,9 +21,9 @@ brew install hyperswitch
 ## Spectacle
 brew install spectacle
 ## Explorer - Chrome - Firefox - Brave
-brew install chrome
+brew install --cask google-chrome
 brew install firefox
-brew install brave
+brew install brave-browser
 
 ## Discord
 brew install discord
@@ -33,14 +33,29 @@ brew install iterm2
 ## NVIM 5
 brew install --HEAD neovim
 
+## brew install nvm and NVM setup
+brew install nvm
+mkdir $HOME/.nvm
+nvm install node
+nvm install --lts
+nvm use node
+nvm run node --version
+cd $HOME
+touch .zshenv
+cat << EOF > .zshenv
+    export NVM_DIR="$HOME/.nvm"
+    NVM_HOMEBREW="/usr/local/opt/nvm/nvm.sh"
+    [ -s "$NVM_HOMEBREW" ] && \. "$NVM_HOMEBREW"
+    [ -x "$(command -v npm)" ] && export NODE_PATH=$NODE_PATH:`npm root -g`
+EOF
 ## Creation of files
 
 # .zprofile
 cd $HOME/
 touch .zprofile
 cat << EOF > .zprofile
-export EDITOR="nvim"
-export ZDOTDIR="$HOME/.config/zsh"
-alias vim="nvim"
-
+    export EDITOR="nvim"
+    export ZDOTDIR="$HOME/.config/zsh"
+    alias vim="nvim"
+EOF
 
